35c11bbfdf8c5a442ef20d2a747da475090c650d
m-pkalimuthu <m-pkalimuthu@DTPC06GYLA>
2019-07-25 17:06:13 +0530

    Azure Functions project.

---
+++ CRM Solution Manager/AzureFunctions/MyFunction/CrmConstants.cs
@@ -0,0 +1,323 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CrmConstants.cs" company="Microsoft">
+//     Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// <author>Syed Amrullah Mazhar</author>
+//-----------------------------------------------------------------------
+
+namespace MyFunction
+{
+    using System;
+    using System.Net;
+    using System.ServiceModel.Description;
+    using System.Threading.Tasks;
+    using Microsoft.IdentityModel.Clients.ActiveDirectory;
+    using Microsoft.Xrm.Sdk;
+    using Microsoft.Xrm.Sdk.Client;
+    using Microsoft.Xrm.Sdk.Query;
+    using Microsoft.Xrm.Sdk.WebServiceClient;
+
+    /// <summary>
+    /// class contains constants for dynamics
+    /// </summary>
+    public class CrmConstants : ConfigurationSettings
+    {
+        /// <summary>
+        /// Solution Packager Path
+        /// </summary>
+        private string solutionPackagerPath;
+
+        /// <summary>
+        /// Multiple Solutions Import
+        /// </summary>
+        private string multilpleSolutionsImport;
+
+        /// <summary>
+        /// Solution To Be Imported
+        /// </summary>
+        private string solutionToBeImported;
+
+        /// <summary>
+        /// Solution Packager Relative Path
+        /// </summary>
+        private string solutionPackagerRelativePath;
+
+        /// <summary>
+        /// Sleep Timeout In Millis
+        /// </summary>
+        private string sleepTimeoutInMillis;
+
+        /// <summary>
+        /// Client Id
+        /// </summary>
+        private string clientId;
+
+        /// <summary>
+        /// Client Secret
+        /// </summary>
+        private string clientSecret;
+
+        /// <summary>
+        /// Tenant Id
+        /// </summary>
+        private string tenantId;
+
+        /// <summary>
+        /// Org Url
+        /// </summary>
+        private string orgUrl;
+
+        /// <summary>
+        /// SDK Service
+        /// </summary>
+        private OrganizationWebProxyClient sdkService;
+
+        /// <summary>
+        /// org service
+        /// </summary>
+        private IOrganizationService service;
+
+        /// <summary>
+        /// Gets client id
+        /// </summary>
+        public string ClientId
+        {
+            get
+            {
+                return this.clientId;
+            }
+        }
+
+        /// <summary>
+        /// Gets client secret
+        /// </summary>
+        public string ClientSecret
+        {
+            get
+            {
+                return this.clientSecret;
+            }
+        }
+
+        /// <summary>
+        /// Gets tenant id
+        /// </summary>
+        public string TenantId
+        {
+            get
+            {
+                return this.tenantId;
+            }
+        }
+
+        /// <summary>
+        /// Gets org url
+        /// </summary>
+        public string OrgUrl
+        {
+            get
+            {
+                return this.orgUrl;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets solution packager path
+        /// </summary>
+        public string SolutionPackagerPath
+        {
+            get
+            {
+                return this.solutionPackagerPath;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets MultipleSolutionsImport PS path
+        /// </summary>
+        public string MultilpleSolutionsImport
+        {
+            get
+            {
+                return this.multilpleSolutionsImport;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets SolutionToBeImported PS path
+        /// </summary>
+        public string SolutionToBeImported
+        {
+            get
+            {
+                return this.solutionToBeImported;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets solution packager relative path
+        /// </summary>
+        public string SolutionPackagerRelativePath
+        {
+            get
+            {
+                return this.solutionPackagerRelativePath;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets sleep timeout in millis
+        /// </summary>
+        public string SleepTimeoutInMillis
+        {
+            get
+            {
+                return this.sleepTimeoutInMillis;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Organization Service Proxy
+        /// </summary>
+        public IOrganizationService ServiceProxy
+        {
+            get
+            {
+                return this.service;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Method returns configuration settings entity collection list
+        /// </summary>
+        /// <param name="orgUrl">org url</param>
+        /// <param name="clientId">client id</param>
+        /// <param name="clientSecret">client secret</param>
+        /// <param name="tenantId">tenant id</param>
+        /// <returns>returns entity collection</returns>
+        public override EntityCollection GetConfigurationSettings(string orgUrl, string clientId, string clientSecret, string tenantId)
+        {
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+            this.tenantId = tenantId;
+            this.orgUrl = orgUrl;
+
+            Task<string> callTask = Task.Run(() => this.AccessTokenGenerator());
+            callTask.Wait();
+            string token = callTask.Result;
+            Uri serviceUrl = new Uri(this.orgUrl + @"/xrmservices/2011/organization.svc/web?SdkClientVersion=8.2");
+
+            using (this.sdkService = new OrganizationWebProxyClient(serviceUrl, false))
+            {
+                this.sdkService.HeaderToken = token;
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                this.service = (IOrganizationService)this.sdkService != null ? (IOrganizationService)this.sdkService : null;
+            }
+            
+            EntityCollection retrievedConfigurationSettingsList = this.RetrieveConfigurationSettings(this.service);
+
+            return retrievedConfigurationSettingsList;
+        }       
+
+        /// <summary>
+        /// Method sets CRM constant property values
+        /// </summary>
+        /// <param name="retrievedConfigurationSettingsList">entity collection</param>
+        public override void SetCrmProperties(EntityCollection retrievedConfigurationSettingsList)
+        {
+            foreach (Entity setting in retrievedConfigurationSettingsList.Entities)
+            {
+                string key = setting.GetAttributeValue<string>("syed_name");
+
+                switch (key)
+                {
+                    case Constants.SolutionPackagerPath:
+                        this.solutionPackagerPath = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    case Constants.MultilpleSolutionsImport:
+                        this.multilpleSolutionsImport = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    case Constants.SolutionToBeImported:
+                        this.solutionToBeImported = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    case Constants.SolutionPackagerRelativePath:
+                        this.solutionPackagerRelativePath = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    case Constants.SleepTimeoutInMillis:
+                        this.sleepTimeoutInMillis = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method generated access token to authenticate dynamics CRM
+        /// </summary>
+        /// <returns>returns access token</returns>
+        private async Task<string> AccessTokenGenerator()
+        {
+            string authority = "https://login.microsoftonline.com/" + this.tenantId;
+
+            var credentials = new ClientCredential(this.clientId, this.clientSecret);
+            var authContext = new AuthenticationContext(authority);
+            var result = await authContext.AcquireTokenAsync(this.orgUrl, credentials);
+            return result.AccessToken;
+        }
+
+        /// <summary>
+        /// Method retrieves active configuration settings record
+        /// </summary>
+        /// <param name="serviceProxy">organization service proxy</param>
+        /// <returns>returns entity collection</returns>
+        private EntityCollection RetrieveConfigurationSettings(IOrganizationService serviceProxy)
+        {
+            try
+            {
+                string fetchXML = @"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'>
+                                    <entity name = 'syed_configurationsettings'>
+                                    <attribute name = 'syed_name' />
+                                    <attribute name = 'syed_value' />
+                                    <attribute name = 'syed_configurationsettingsid' />
+                                    <order descending = 'false' attribute = 'syed_name'/>
+                                    <filter type = 'and'>
+                                    <condition attribute = 'statecode' value = '0' operator= 'eq' />
+                                    </filter>
+                                    </entity>
+                                    </fetch>";
+
+                EntityCollection configurationSettingsList = serviceProxy.RetrieveMultiple(new FetchExpression(fetchXML));
+                return configurationSettingsList;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message.ToString(), ex);
+            }
+        }      
+    }
+}
---
+++ CRM Solution Manager/AzureFunctions/MyFunction/RepositoryConfigurationConstants.cs
@@ -0,0 +1,369 @@
+//-----------------------------------------------------------------------
+// <copyright file="RepositoryConfigurationConstants.cs" company="Microsoft">
+//     Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+// <author>Syed Amrullah Mazhar</author>
+//-----------------------------------------------------------------------
+
+namespace MyFunction
+{
+    using System;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.IO;
+    using Microsoft.Xrm.Sdk;
+
+    /// <summary>
+    /// constants file for repository configurations
+    /// </summary>
+    public class RepositoryConfigurationConstants : ConfigurationSettings
+    {
+        /// <summary>
+        /// Method substitutes drive
+        /// </summary>
+        public const string SubstDrive = "k";
+
+        /// <summary>
+        /// Solution Folder
+        /// </summary>
+        private string solutionFolder;
+
+        /// <summary>
+        /// JavaScript Directory
+        /// </summary>
+        private string javaScriptDirectory;
+
+        /// <summary>
+        /// Html Directory
+        /// </summary>
+        private string htmlDirectory;
+
+        /// <summary>
+        /// Images Directory
+        /// </summary>
+        private string imagesDirectory;
+
+        /// <summary>
+        /// Repository Url
+        /// </summary>
+        private string repositoryUrl;
+
+        /// <summary>
+        /// Clone Repository Always
+        /// </summary>
+        private string cloneRepositoryAlways;
+
+        /// <summary>
+        /// Repository Remote Name
+        /// </summary>
+        private string repositoryRemoteName;
+
+        /// <summary>
+        /// Branch Name
+        /// </summary>
+        private string branchName;
+
+        /// <summary>
+        /// Gets or sets Repository local directory
+        /// </summary>
+        public string LocalDirectory
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets repository url
+        /// </summary>
+        public string GitUserName
+        {
+            get
+            {
+                return "v-prkali";
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository url
+        /// </summary>
+        public string GitUserPassword
+        {
+            get
+            {
+                return "Shahidkapoor12#";
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository release directory containing CRM Solutions
+        /// </summary>
+        public string SolutionFolder
+        {
+            get
+            {
+                return this.solutionFolder;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository script directory
+        /// </summary>
+        public string JsDirectory
+        {
+            get
+            {
+                return this.javaScriptDirectory;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository html directory
+        /// </summary>
+        public string HtmlDirectory
+        {
+            get
+            {
+                return this.htmlDirectory;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository Images directory
+        /// </summary>
+        public string ImagesDirectory
+        {
+            get
+            {
+                return this.imagesDirectory;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository url
+        /// </summary>
+        public string RepositoryUrl
+        {
+            get
+            {
+                return this.repositoryUrl;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository url
+        /// </summary>
+        public string CloneRepositoryAlways
+        {
+            get
+            {
+                return this.cloneRepositoryAlways;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets repository remote name
+        /// </summary>
+        public string RepositoryRemoteName
+        {
+            get
+            {
+                return this.repositoryRemoteName;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets Repository branch name
+        /// </summary>
+        public string BranchName
+        {
+            get
+            {
+                return this.branchName;
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// method resets local directory
+        /// </summary>
+        public void ResetLocalDirectory()
+        {
+            // LocalDirectory = @"\\?\" + Path.GetTempFileName().Replace(".","") + "devopsTmp\\";
+            this.LocalDirectory = Path.GetTempFileName().Replace(".", string.Empty) + "devopsTmp\\";
+            Singleton.CrmSolutionHelperInstance.CreateEmptyFolder(this.LocalDirectory);
+            this.SubstTempDirectory();
+        }
+
+        /// <summary>
+        /// Method sets repository configuration constant property values
+        /// </summary>
+        /// <param name="retrievedConfigurationSettingsList">entity collection</param>
+        public override void SetRepositoryConfigurationProperties(EntityCollection retrievedConfigurationSettingsList)
+        {
+            foreach (Entity setting in retrievedConfigurationSettingsList.Entities)
+            {
+                string key = setting.GetAttributeValue<string>("syed_name");
+
+                switch (key)
+                {
+                    case Constants.RepositorySolutionFolder:
+                        if (!string.IsNullOrEmpty(setting.GetAttributeValue<string>("syed_value")))
+                        {
+                            this.solutionFolder = Path.Combine(this.LocalDirectory, setting.GetAttributeValue<string>("syed_value"));
+                        }
+
+                        break;
+                    case Constants.RepositoryJsDirectory:
+                        if (!string.IsNullOrEmpty(setting.GetAttributeValue<string>("syed_value")))
+                        {
+                            this.javaScriptDirectory = Path.Combine(this.LocalDirectory, setting.GetAttributeValue<string>("syed_value"));
+                        }
+
+                        break;
+                    case Constants.RepositoryHtmlDirectory:
+                        if (!string.IsNullOrEmpty(setting.GetAttributeValue<string>("syed_value")))
+                        {
+                            this.htmlDirectory = Path.Combine(this.LocalDirectory, setting.GetAttributeValue<string>("syed_value"));
+                        }
+
+                        break;
+                    case Constants.RepositoryImagesDirectory:
+                        if (!string.IsNullOrEmpty(setting.GetAttributeValue<string>("syed_value")))
+                        {
+                            this.imagesDirectory = Path.Combine(this.LocalDirectory, setting.GetAttributeValue<string>("syed_value"));
+                        }
+
+                        break;
+                    case Constants.RepositoryUrl:
+                        this.repositoryUrl = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    case Constants.CloneRepositoryAlways:
+                        this.cloneRepositoryAlways = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    case Constants.RemoteName:
+                        this.repositoryRemoteName = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    case Constants.BranchName:
+                        this.branchName = setting.GetAttributeValue<string>("syed_value");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// method substitutes temporary directory
+        /// </summary>
+        private void SubstTempDirectory()
+        {
+            try
+            {
+                if (Directory.Exists(SubstDrive + ":\\"))
+                {
+                    this.DeleteSubstTempDirectory();
+                }
+
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "subst",
+                    Arguments = SubstDrive + ": '" + this.LocalDirectory + "'"
+                };
+                Process.Start(startInfo);
+
+                Process process = new Process();
+                process.StartInfo.FileName = "subst.exe";
+                process.StartInfo.Arguments = " " + SubstDrive + ": \"" + this.LocalDirectory.Remove(this.LocalDirectory.Length - 1, 1) + "\"";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+
+                string output = process.StandardOutput.ReadToEnd();
+                Singleton.SolutionFileInfoInstance.WebJobsLog.AppendLine(" " + output + "<br>");
+                string err = process.StandardError.ReadToEnd();
+                Singleton.SolutionFileInfoInstance.WebJobsLog.AppendLine(" " + err + "<br>");
+                process.WaitForExit();
+
+                Directory.CreateDirectory(SubstDrive + ":\\1\\");
+                this.LocalDirectory = SubstDrive + ":\\1\\";
+            }
+            catch (Exception ex)
+            {
+                Singleton.SolutionFileInfoInstance.WebJobsLog.AppendLine(" " + ex.Message + "<br>");
+            }
+        }
+
+        /// <summary>
+        /// method deletes substituted temporary directory
+        /// </summary>
+        private void DeleteSubstTempDirectory()
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "subst",
+                    Arguments = SubstDrive + ": '" + this.LocalDirectory + "'"
+                };
+                Process.Start(startInfo);
+
+                Process process = new Process();
+                process.StartInfo.FileName = "subst.exe";
+                process.StartInfo.Arguments = " " + SubstDrive + ": /d";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                Singleton.SolutionFileInfoInstance.WebJobsLog.AppendLine(" " + output + "<br>");                
+                string err = process.StandardError.ReadToEnd();
+                Singleton.SolutionFileInfoInstance.WebJobsLog.AppendLine(" " + err + "<br>");               
+                process.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                Singleton.SolutionFileInfoInstance.WebJobsLog.AppendLine(ex.Message + "<br>");               
+            }
+        }
+    }
+}

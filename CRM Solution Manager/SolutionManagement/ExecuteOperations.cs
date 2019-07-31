acca33459f57cb60970d7c69d8b5bc5ad86c5519
Jaiyanthi Hari Krishnan <v-jaikri@microsoft.com>
2019-07-31 17:28:44 +0530

    Date time issue fixed

--- CRM Solution Manager/SolutionManagement/ExecuteOperations.cs
+++ CRM Solution Manager/SolutionManagement/ExecuteOperations.cs
@@ -104,8 +104,8 @@ namespace SolutionManagement
             sourcecontrolqueue.syed_Status = "Draft";
             sourcecontrolqueue.syed_CheckInBySolution = true;
             sourcecontrolqueue.syed_CheckInBySolutionId = solutionId;
-            sourcecontrolqueue.syed_name = "SOL-" + DateTime.Now.ToString();
-            sourcecontrolqueue.syed_Comment = mode + DateTime.Now.ToString();
+            sourcecontrolqueue.syed_name = "SOL-" + DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm:ss");
+            sourcecontrolqueue.syed_Comment = mode + DateTime.UtcNow.ToString("dd/MM/yyyy hh:mm:ss");
             sourcecontrolqueue.syed_overwritesolutionstxt = new OptionSetValue(433710000);
             sourcecontrolqueue.syed_CheckIn = true;
             if (mode == "Release")

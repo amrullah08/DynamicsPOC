9cfca75269d04e4c06a88ad4251604bb771fbd79
amrullah08 <35560136+amrullah08@users.noreply.github.com>
2020-03-29 01:03:04 +0530

    updated webresources

--- CRM Solution Manager/CRM Solution/Scripts/MergeSoluitons/EventHandler.js
+++ CRM Solution Manager/CRM Solution/Scripts/MergeSoluitons/EventHandler.js
@@ -1,73 +1,85 @@
-if (typeof (SYED) === "undefined") {
+if (typeof SYED === 'undefined') {
     SYED = { __namespace: true };
-}
-if (typeof (SYED.MergeSolution) === "undefined") {
+  }
+  if (typeof SYED.MergeSolution === 'undefined') {
     SYED.MergeSolution = { __namespace: true };
-}
-
-'use strict';
-SYED.MergeSolution.EventHandler =
-    {
-        SetCRMSolutionValues: function (executionContext) {
-            try {
-                var formContext = executionContext.getFormContext();
-                var sourceControlValues = formContext.getAttribute("syed_crmsolutions").getValue();
-                if (sourceControlValues != null || sourceControlValues != undefined) {
-                    sourceControlValues = sourceControlValues[0].id.replace("{", "").replace("}", "").toUpperCase();
-
-                    Xrm.WebApi.online.retrieveRecord("syed_mastersolutions", sourceControlValues, "?$select=syed_friendlyname,syed_installedon,syed_ismanaged,syed_listofsolutions,syed_mastersolutionsid,syed_name,syed_order,syed_publisher,syed_solutionid,syed_solutioninstalledon,syed_version,syed_versionnumber").then(
-                        function success(result) {
-                            formContext.getAttribute("syed_syed_friendlyname").setValue(result["syed_friendlyname"]);
-                            formContext.getAttribute("syed_ismanaged").setValue(result["syed_ismanaged"]);
-                            formContext.getAttribute("syed_name").setValue(result["syed_name"]);
-                            formContext.getAttribute("syed_publisher").setValue(result["syed_publisher"]);
-                            formContext.getAttribute("syed_solutioninstalledon").setValue(new Date(result["syed_solutioninstalledon"]));
-                            formContext.getAttribute("syed_uniquename").setValue(result["syed_listofsolutions"]);
-                            formContext.getAttribute("syed_version").setValue(result["syed_version"]);
-
-                            formContext.getAttribute("syed_order").setValue(0);
-                        },
-                        function (error) {
-                            Xrm.Utility.alertDialog(error.message);
-                        }
-                    );
-                }
-                else {
-                    if (!Xrm.Internal.isUci()) {
-                        formContext.getAttribute("syed_syed_friendlyname").setValue("");
-                        formContext.getAttribute("syed_ismanaged").setValue(false);
-                        formContext.getAttribute("syed_name").setValue("");
-                        formContext.getAttribute("syed_publisher").setValue("");
-                        formContext.getAttribute("syed_solutioninstalledon").setValue("");
-                        formContext.getAttribute("syed_uniquename").setValue("");
-                        formContext.getAttribute("syed_version").setValue("");
-                        formContext.getAttribute("syed_order").setValue(0);
-                    }
-                }
-            }
-            catch (ex) {
-                console.log("Error at SYED.MergeSolution.EventHandler.SetCRMSolutionValues function: " + ex.message + "|" + "Stack: " + ex.stack);
-                throw ex;
-            }
-        },
-
-        Back: function (executionContext) {
-            try {
-                if (Xrm.Internal.isUci())
-                    formContext = executionContext;
-                else
-                    formContext = executionContext.getFormContext();
-
-                var sourceControl = formContext.getAttribute("syed_mastersolution").getValue();
-                if (sourceControl != null || sourceControl != undefined) {
-                    var entityFormOptions = {};
-                    entityFormOptions["entityName"] = sourceControl[0].entityType;
-                    entityFormOptions["entityId"] = sourceControl[0].id;
-                    Xrm.Navigation.openForm(entityFormOptions);
-                }
-            } catch (ex) {
-                console.log("Error at SYED.MergeSolution.EventHandler.Back function: " + ex.message + "|" + "Stack: " + ex.stack);
-                throw ex;
-            }
+  }
+  
+  ('use strict');
+  SYED.MergeSolution.EventHandler = {
+    SetCRMSolutionValues: function(executionContext) {
+      try {
+        const formContext = executionContext.getFormContext();
+        let sourceControlValues = formContext.getAttribute('syed_crmsolutions').getValue();
+        if (sourceControlValues != null || sourceControlValues != undefined) {
+          sourceControlValues = sourceControlValues[0].id
+            .replace('{', '')
+            .replace('}', '')
+            .toUpperCase();
+  
+          Xrm.WebApi.online
+            .retrieveRecord(
+              'syed_mastersolutions',
+              sourceControlValues,
+              '?$select=syed_friendlyname,syed_installedon,syed_ismanaged,syed_listofsolutions,syed_mastersolutionsid,syed_name,syed_order,syed_publisher,syed_solutionid,syed_solutioninstalledon,syed_version,syed_versionnumber',
+            )
+            .then(
+              function success(result) {
+                formContext.getAttribute('syed_syed_friendlyname').setValue(result.syed_friendlyname);
+                formContext.getAttribute('syed_ismanaged').setValue(result.syed_ismanaged);
+                formContext.getAttribute('syed_name').setValue(result.syed_name);
+                formContext.getAttribute('syed_publisher').setValue(result.syed_publisher);
+                formContext.getAttribute('syed_solutioninstalledon').setValue(new Date(result.syed_solutioninstalledon));
+                formContext.getAttribute('syed_uniquename').setValue(result.syed_listofsolutions);
+                formContext.getAttribute('syed_version').setValue(result.syed_version);
+  
+                formContext.getAttribute('syed_order').setValue(0);
+              },
+              function(error) {
+                Xrm.Utility.alertDialog(error.message);
+              },
+            );
+        } else {
+          if (!Xrm.Internal.isUci()) {
+            formContext.getAttribute('syed_syed_friendlyname').setValue('');
+            formContext.getAttribute('syed_ismanaged').setValue(false);
+            formContext.getAttribute('syed_name').setValue('');
+            formContext.getAttribute('syed_publisher').setValue('');
+            formContext.getAttribute('syed_solutioninstalledon').setValue('');
+            formContext.getAttribute('syed_uniquename').setValue('');
+            formContext.getAttribute('syed_version').setValue('');
+            formContext.getAttribute('syed_order').setValue(0);
+          }
         }
-    }
+      } catch (ex) {
+        console.log(
+          'Error at SYED.MergeSolution.EventHandler.SetCRMSolutionValues function: ' +
+            ex.message +
+            '|' +
+            'Stack: ' +
+            ex.stack,
+        );
+        throw ex;
+      }
+    },
+  
+    Back: function(executionContext) {
+      try {
+        if (Xrm.Internal.isUci()) formContext = executionContext;
+        else formContext = executionContext.getFormContext();
+  
+        const sourceControl = formContext.getAttribute('syed_mastersolution').getValue();
+        if (sourceControl != null || sourceControl != undefined) {
+          const entityFormOptions = {};
+          entityFormOptions.entityName = sourceControl[0].entityType;
+          entityFormOptions.entityId = sourceControl[0].id;
+  
+          Xrm.Navigation.openForm(entityFormOptions);
+        }
+      } catch (ex) {
+        console.log('Error at SYED.MergeSolution.EventHandler.Back function: ' + ex.message + '|' + 'Stack: ' + ex.stack);
+        throw ex;
+      }
+    },
+  };
+  
--- CRM Solution Manager/CRM Solution/Scripts/Solution/Ribbon.js
+++ CRM Solution Manager/CRM Solution/Scripts/Solution/Ribbon.js
@@ -1,75 +1,78 @@
-﻿if (typeof (SYED) === "undefined") {
+﻿if (typeof SYED === 'undefined') {
     SYED = { __namespace: true };
-}
-if (typeof (SYED.SolutionDetail) === "undefined") {
+  }
+  if (typeof SYED.SolutionDetail === 'undefined') {
     SYED.Solution = { __namespace: true };
-}
-
-'use strict';
-SYED.Solution.Ribbon =
-    {
-        Execute: function (selectedId, mode) {
-            try {
-                debugger;
-                if (selectedId.length > 0) {
-                    selectedId = selectedId[0].replace("{", "").replace("}", "").toUpperCase();
-
-                    Xrm.WebApi.online.retrieveRecord("solution", selectedId, "?$select=ismanaged").then(
-                        function success(result) {
-                            var ismanaged = result["ismanaged"];
-                            if (ismanaged) {
-                                Xrm.Utility.alertDialog("Please select any Unmanaged Solution to process");
-                            }
-                            else {
-                                SYED.Solution.Ribbon.CallAction(selectedId, mode);
-                            }
-                        },
-                        function (error) {
-                            Xrm.Utility.alertDialog(error.message);
-                        }
-                    );
-
-                }
-                else {
-                    Xrm.Utility.alertDialog("Please select any one of Unmanaged Solution to process");
-                }
-
-            }
-            catch (ex) {
-                console.log("Error at SYED.Solution.Ribbon.Execute function: " + ex.message + "|" + "Stack: " + ex.stack);
-                throw ex;
-            }
-        },
-
-        CallAction: function (selectedId, mode) {
-            try {
-                debugger;
-                var parameters = {};
-                parameters.SolutionId = selectedId;
-                parameters.CheckIn = mode;
-
-                var req = new XMLHttpRequest();
-                req.open("POST", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/syed_CreateDynamicsSourceControlBySolution", false);
-                req.setRequestHeader("OData-MaxVersion", "4.0");
-                req.setRequestHeader("OData-Version", "4.0");
-                req.setRequestHeader("Accept", "application/json");
-                req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-                req.onreadystatechange = function () {
-                    if (this.readyState === 4) {
-                        req.onreadystatechange = null;
-                        if (this.status === 200) {
-                            var results = JSON.parse(this.response);
-                            Xrm.Utility.alertDialog(mode + "- is in progress, for more details, Please refer Dynamics Source Control record");
-                        } else {
-                            Xrm.Utility.alertDialog(this.statusText);
-                        }
-                    }
-                };
-                req.send(JSON.stringify(parameters));
-            }
-            catch (ex) {
-                console.log("Error at SYED.Solution.Ribbon.CallAction function: " + ex.message + "|" + "Stack: " + ex.stack);
-                throw ex;
-            }
+  }
+  
+  ('use strict');
+  SYED.Solution.Ribbon = {
+    Execute: function(selectedId, mode) {
+      try {
+        debugger;
+        if (selectedId.length > 0) {
+          selectedId = selectedId[0]
+            .replace('{', '')
+            .replace('}', '')
+            .toUpperCase();
+  
+          Xrm.WebApi.online.retrieveRecord('solution', selectedId, '?$select=ismanaged').then(
+            function success(result) {
+              const ismanaged = result.ismanaged;
+              if (ismanaged) {
+                Xrm.Utility.alertDialog('Please select any Unmanaged Solution to process');
+              } else {
+                SYED.Solution.Ribbon.CallAction(selectedId, mode);
+              }
+            },
+            function(error) {
+              Xrm.Utility.alertDialog(error.message);
+            },
+          );
+        } else {
+          Xrm.Utility.alertDialog('Please select any one of Unmanaged Solution to process');
         }
-    }
+      } catch (ex) {
+        console.log('Error at SYED.Solution.Ribbon.Execute function: ' + ex.message + '|' + 'Stack: ' + ex.stack);
+        throw ex;
+      }
+    },
+  
+    CallAction: function(selectedId, mode) {
+      try {
+        debugger;
+        const parameters = {};
+        parameters.SolutionId = selectedId;
+        parameters.CheckIn = mode;
+  
+        const req = new XMLHttpRequest();
+        req.open(
+          'POST',
+          Xrm.Page.context.getClientUrl() + '/api/data/v9.1/syed_CreateDynamicsSourceControlBySolution',
+          false,
+        );
+        req.setRequestHeader('OData-MaxVersion', '4.0');
+        req.setRequestHeader('OData-Version', '4.0');
+        req.setRequestHeader('Accept', 'application/json');
+        req.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        req.onreadystatechange = function() {
+          if (this.readyState === 4) {
+            req.onreadystatechange = null;
+            if (this.status === 200) {
+              const results = JSON.parse(this.response);
+              Xrm.Utility.alertDialog(
+                mode + '- is in progress, for more details, Please refer Dynamics Source Control record',
+              );
+            } else {
+              Xrm.Utility.alertDialog(this.statusText);
+            }
+          }
+        };
+        req.send(JSON.stringify(parameters));
+      } catch (ex) {
+        console.log('Error at SYED.Solution.Ribbon.CallAction function: ' + ex.message + '|' + 'Stack: ' + ex.stack);
+        throw ex;
+      }
+    },
+  };
+  
